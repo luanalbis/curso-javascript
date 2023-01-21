da7223c93e6a02359b43fe4b3f4d443b9120adf5
LuanAlbis <luanalbis12@gmail.com>
2023-01-20 21:02:41 -0300

    atualização de exercícios

---
+++ CFB_Cursos/aula01-variaveis/aula01-variaveist.js
@@ -0,0 +1,13 @@
+/// Javascript modo strict, modo para você ter mais cuidado com o código e deixálo mais limpo
+
+'use strict' // isso faz usar o modo estito
+
+let nome = 'Luan' // variavel que respeita o seu escopo, recomendável usa-la
+nome = 10
+console.log(nome)
+
+const curso = 'curso' //Váriavel que não é possível mudar o valor
+
+console.log (curso)
+
+// podemos adicionar varias variaveis no mesmo LET, VAR ou CONST, separando por virgula e dando seu valor ---- exemplo ---- LET nome = 0,curso = 'curso
