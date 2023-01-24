54b1be99c1c18d2d5e6956bb4c9fc5635c644c5f
LuanAlbis <luanalbis12@gmail.com>
2023-01-24 19:41:41 -0300

    Update avaliacao-curto-circuito.js

--- curso-luizotavio-udemy/02-curto-circuito/avaliacao-curto-circuito.js
+++ curso-luizotavio-udemy/02-curto-circuito/avaliacao-curto-circuito.js
@@ -16,3 +16,22 @@ NaN
 */
 
 console.log('Luiz' && true && 'amanda') // se não tiver valor falso, retorna o ultimo valor
+
+function falaOi(){
+    return 'oi luan'
+}
+
+let vaiExecutar = true
+
+console.log(vaiExecutar && falaOi()) // assim podemos fazer uma validação de curto circuito, se for falso irá retornar o valor falso
+
+console.log(null || 'Luan' || 8 || falaOi()) // quando utilizamos o ||(OR) // aqui irá retornar o primeiro valor verdadeiro
+
+let escolhaUsuario = ''
+console.log(escolhaUsuario || "preto") // aqui o valor retornado será preto, pois o usuário se enquadra nos valores FALSY
+
+let hora = new Date().getMilliseconds()
+console.log(hora)
+
+console.log(hora % 2 === 0? 'par': 'impar') // primeiro colocamos a condição, depois o reterno de true , depois o retorno se for falso
+
