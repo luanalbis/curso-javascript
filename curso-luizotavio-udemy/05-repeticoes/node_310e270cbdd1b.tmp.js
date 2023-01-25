db6e761064a55e124a690bf4963fb5b4ef9f4f1d
LuanAlbis <luanalbis12@gmail.com>
2023-01-24 23:18:47 -0300

    att

---
+++ curso-luizotavio-udemy/05-repeticoes/node_310e270cbdd1b.tmp.js
@@ -0,0 +1,17 @@
+const nome = 'Luan da silva albis'
+const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+
+console.log(caracteres)
+
+for (let c = 0; c < nome.length; c++) {
+    if (caracteres.indexOf(nome[c]) < 0){
+        console.log(nome.replace(nome[c],''))
+        c--
+        
+        
+    }
+    
+}
+
+console.log(nome)
