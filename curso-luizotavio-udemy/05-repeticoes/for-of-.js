23d225282d557f2621807a6ba5d25588d131c673
LuanAlbis <luanalbis12@gmail.com>
2023-01-25 00:36:48 -0300

    Update for-of-.js

--- curso-luizotavio-udemy/05-repeticoes/for-of-.js
+++ curso-luizotavio-udemy/05-repeticoes/for-of-.js
@@ -1,13 +1,11 @@
 function palindrome(str) {
-    let nome = str.toString()
+    let nome = str
     let palidromo = ''
     const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
   
     for (let c = 0; c < nome.length; c++) {
         if (caracteres.indexOf(nome[c]) < 0){
             nome = nome.toLowerCase().replaceAll(nome[c],'')
-            
-
         }
     }
   
@@ -19,5 +17,11 @@ function palindrome(str) {
     return palidromo === nome
     
   }
-  
   palindrome("0_0 (: /-\ :) 0-0");
+
+let M=1000,CM=900,D=500,CD=400,C=100,XC=90,L=50,XL=40,X=10,IX=9,V=5,IV=4,I=1
+function convertToRoman(num) {
+ return num;
+}
+
+convertToRoman(36);
