f83deb5081aa2b45fef061c313b3a57063fcf527
LuanAlbis <luanalbis12@gmail.com>
2023-01-25 00:05:08 -0300

    Update for-of-.js

--- curso-luizotavio-udemy/05-repeticoes/for-of-.js
+++ curso-luizotavio-udemy/05-repeticoes/for-of-.js
@@ -1,18 +1,23 @@
-let nome = 'asa'
-const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+function palindrome(str) {
+    let nome = str.toString()
+    let palidromo = ''
+    const caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  
+    for (let c = 0; c < nome.length; c++) {
+        if (caracteres.indexOf(nome[c]) < 0){
+            nome = nome.toLowerCase().replaceAll(nome[c],'')
+            
 
-
-console.log(caracteres)
-
-for (let c = 0; c < nome.length; c++) {
-    if (caracteres.indexOf(nome[c]) < 0){
-        nome = nome.toLowerCase().replaceAll(nome[c],'')
-        
+        }
     }
-}
+  
+    for (let c = nome.length-1; c >= 0 ; c--){
+        palidromo += nome[c]
 
-if (nome.lastIndexOf(nome) === nome.indexOf(nome) ){
-    console.log(nome + 'é um palindromo')
-}
-
-
+    }
+    console.log(palidromo,nome)
+    return palidromo === nome
+    
+  }
+  
+  palindrome("0_0 (: /-\ :) 0-0");
