0941dcfe9a958bc8264340d9b528f8ab50e5ee1c
LuanAlbis <luanalbis12@gmail.com>
2023-01-25 23:56:32 -0300

    att

--- curso-luizotavio-udemy/05-repeticoes/for-of-.js
+++ curso-luizotavio-udemy/05-repeticoes/for-of-.js
@@ -19,9 +19,3 @@ function palindrome(str) {
   }
   palindrome("0_0 (: /-\ :) 0-0");
 
-let M=1000,CM=900,D=500,CD=400,C=100,XC=90,L=50,XL=40,X=10,IX=9,V=5,IV=4,I=1
-function convertToRoman(num) {
- return num;
-}
-
-convertToRoman(36);
