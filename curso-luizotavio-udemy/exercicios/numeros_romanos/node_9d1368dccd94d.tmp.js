5f7bed5580fb279de2bfd0b2a06959a3f3a3d918
LuanAlbis <luanalbis12@gmail.com>
2023-01-26 00:00:40 -0300

    att

--- curso-luizotavio-udemy/exercicios/numeros_romanos/node_9d1368dccd94d.tmp.js
+++ curso-luizotavio-udemy/exercicios/numeros_romanos/node_9d1368dccd94d.tmp.js
@@ -1,10 +1,12 @@
 let M=1000,CM=900,D=500,CD=400,C=100,XC=90,L=50,XL=40,X=10,IX=9,V=5,IV=4,I=1
 let valor = 0
 function convertToRoman(num) {
-    
- return num.convertToRoman();
+    while (valor = num){
+
+    }
+ return num;
 }
 
 
 
-console.log(convertToRoman(36));
+convertToRoman(36);
