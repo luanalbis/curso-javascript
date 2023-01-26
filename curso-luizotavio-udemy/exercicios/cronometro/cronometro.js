21f2a3f4868d55b2f4bfcd4a62699a4e8d370d21
LuanAlbis <luanalbis12@gmail.com>
2023-01-26 19:06:22 -0300

    att

--- curso-luizotavio-udemy/exercicios/cronometro/cronometro.js
+++ curso-luizotavio-udemy/exercicios/cronometro/cronometro.js
@@ -1,12 +1,13 @@
-let minuto = 0, segundo = 0, milisegundo = 0, aceleracao = 0
+let minuto = 0, segundo = 0, milisegundo = 0, aceleracao = 0,c = 0
 
 function tempo() {
+    
     if (milisegundo == 100) {
         milisegundo = 0
         segundo++
     } else if (segundo == 60){
         segundo = 0
-        minuto++
+        minuto++ 
     }
     
     if (segundo < 10){
@@ -17,24 +18,23 @@ function tempo() {
 }
 
 function iniciar (){ 
-    timer = setInterval(() => {
-        tempo()},10)
-    aceleracao ++
-    if (aceleracao>1){
-        document.getElementById('iniciar').innerHTML = `${aceleracao}`
-    }
+    if (c == 0){
+        timer = setInterval(() => {tempo()} ,10)
+        document.getElementById('cronometro').style.color = "black" 
+    }c++
 } 
 
 function parar () {
     setTimeout(() => {
         clearInterval(timer)
     },0)
+    document.getElementById('cronometro').style.color = "rgb(238, 11, 11)"
+    c = 0
 }
 
 function zerar () {
     document.getElementById('cronometro').innerHTML = '00:00:00'
     milisegundo = 0,  segundo = 0, minuto = 0
-
 }
 
 
