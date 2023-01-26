2dd78e272c17300cf5485401deb9dfe6e7416a66
LuanAlbis <luanalbis12@gmail.com>
2023-01-26 17:04:23 -0300

    atualização exrecício do cronômetro

--- curso-luizotavio-udemy/exercicios/cronometro/cronometro.js
+++ curso-luizotavio-udemy/exercicios/cronometro/cronometro.js
@@ -1,20 +1,45 @@
-let minuto = 0, segundo = 0, milisegundo = 0
+let minuto = 0, segundo = 0, milisegundo = 0, aceleracao = 0
 
-function iniciar (){
+function tempo() {
+    if (milisegundo == 100) {
+        milisegundo = 0
+        segundo++
+    } else if (segundo == 60){
+        segundo = 0
+        minuto++
+    }
     
+    if (segundo < 10){
+        document.getElementById('cronometro').innerHTML=`0${minuto}:0${segundo}:${milisegundo++}`
+    } else {
+        document.getElementById('cronometro').innerHTML=`0${minuto}:${segundo}:${milisegundo++}`
+    }
 }
 
+function iniciar (){ 
+    timer = setInterval(() => {
+        tempo()},10)
+    aceleracao ++
+    if (aceleracao>1){
+        document.getElementById('iniciar').innerHTML = `${aceleracao}`
+    }
+} 
+
 function parar () {
-    return 0
+    setTimeout(() => {
+        clearInterval(timer)
+    },0)
 }
 
 function zerar () {
     document.getElementById('cronometro').innerHTML = '00:00:00'
+    milisegundo = 0,  segundo = 0, minuto = 0
+
 }
 
-timer = setInterval(() => {
-},100)
 
-setTimeout(() => {
-    clearInterval(timer)
-},0)
+
+
+
+
+
