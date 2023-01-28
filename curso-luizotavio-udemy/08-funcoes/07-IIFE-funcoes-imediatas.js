65f721a086d1155aa7fe35e57797df6642df827c
LuanAlbis <luanalbis12@gmail.com>
2023-01-27 21:30:52 -0300

    atualização

--- curso-luizotavio-udemy/08-funcoes/07-IIFE-funcoes-imediatas.js
+++ curso-luizotavio-udemy/08-funcoes/07-IIFE-funcoes-imediatas.js
@@ -1,1 +1,11 @@
 // funções chamamas imediatamente, criadas para nossas váriaveis não tocare
+
+// a função precisa ter esta sintaxe
+(function(){
+    let nome = 'paulo' // variavel da função IIFE, nod protejendo do escopo global
+    console.log(nome)
+})()
+
+let nome = 'luan' // variavel do escopo global
+
+console.log(nome)
