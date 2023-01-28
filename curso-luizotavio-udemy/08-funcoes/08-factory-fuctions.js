d87a1575a31f70617d9c278910e4372673237a20
LuanAlbis <luanalbis12@gmail.com>
2023-01-27 22:25:12 -0300

    Update 08-factory-fuctions.js

--- curso-luizotavio-udemy/08-funcoes/08-factory-fuctions.js
+++ curso-luizotavio-udemy/08-funcoes/08-factory-fuctions.js
@@ -4,9 +4,13 @@ function criaPessoa(nome,sobrenome){
     return {
         nome,
         sobrenome,
-        fala: (assunto) => {return `${nome} ${assunto}`}
+
+        // getter não recebe parâmetros
+        get nomeCompleto() {return `${nome} ${sobrenome}`}
+
+        //setter recebe parâmetros
     }
 }
 
-let pessoa = criaPessoa('luan', 'amanda') // { nome:'luan, sobrenome:'albis', fala: function }
-console.log(pessoa.fala('estuda bastante'))
+let pessoa = criaPessoa('luan', 'albis') // { nome:'luan, sobrenome:'albis', fala: function }
+console.log(pessoa.nomeCompleto) // get função faz a função se passar por um item sem precisar de ()
