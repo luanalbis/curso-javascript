8d923df9794f45705ba7a259ca0699ac5212a130
LuanAlbis <luanalbis12@gmail.com>
2023-01-30 20:54:44 -0300

    Update 06-reduce.js

--- curso-luizotavio-udemy/03-arrays/06-reduce.js
+++ curso-luizotavio-udemy/03-arrays/06-reduce.js
@@ -4,4 +4,20 @@
 
 const numeros = [1,26,77,88,9,8,634,90,8,7,23]
 const total = numeros.reduce((acumulador,valor,indice,array) => acumulador+= valor,0)
-console.log(total)
+//console.log(total)
+
+
+const pessoas = [
+    {nome:'Luan',idade:22},
+    {nome:'Amanda',idade:21},
+    {nome:'Paulo',idade:38},
+    {nome:'cauan',idade:18},
+    {nome:'Rejiana',idade:38}
+]
+
+const maisVelho = pessoas.reduce((acumulador,valor) => {
+    if (acumulador.idade > valor.idade) return acumulador
+    return valor
+})
+
+console.log(maisVelho)
