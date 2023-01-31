df5af68503929b21515e3a5a3fd10dc96186fedb
LuanAlbis <luanalbis12@gmail.com>
2023-01-31 20:57:16 -0300

    att

--- curso-luizotavio-udemy/01-Fundamentos/04-objetos/06-heranca-prototype.js
+++ curso-luizotavio-udemy/01-Fundamentos/04-objetos/06-heranca-prototype.js
@@ -2,9 +2,10 @@
 // Camiseta, Caneca, Lapis
 // cada produto pode ter características específicas
 
-function Produto(nome,preco) {
+function Produto(nome,preco,cor) {
     this.nome = nome
     this.preco = preco
+    this.cor = cor
 }
 
 Produto.prototype.aumento = function(quantia) {
@@ -14,9 +15,27 @@ Produto.prototype.desconto = function(quantia) {
     this.preco -= quantia
 }
 
-function Camiseta(nome,preco,cor) {
-    Produto.call(this,nome,preco)
+function Camiseta(nome,preco,cor) { 
+    Produto.call(this,nome,preco,cor) // camiseta herda tudo do Produto, após o this.
 }
 
-const camiseta = new Camiseta('Regata', 10)
-camiseta.aumento(10)
+Camiseta.prototype = Object.create(Produto.prototype) // defino o prototype o mesmo do Produto
+Camiseta.prototype.constructor = Camiseta // define o construtor da Camiseta para ela mesma
+
+function Caneca (nome,preco,cor,marca) {
+    Produto.call(this,nome,preco,cor)
+    this.marca = marca
+}
+
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca
+
+const produto = new Produto('Gen', 111 , 'preto')
+const camiseta = new Camiseta('Blusa', 50)
+const caneca = new Caneca('caneca', 50,'branca','naruto mania')
+
+console.log(produto)
+console.log(camiseta)
+caneca.aumento(20)// aqui o preço da caneca irá auterar pois o prototype tem a função de aumentar
+console.log(caneca)
+
