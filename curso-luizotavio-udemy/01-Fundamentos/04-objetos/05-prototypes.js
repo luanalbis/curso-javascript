ed852e547dec376c84875a76c9701e8e727ad095
LuanAlbis <luanalbis12@gmail.com>
2023-01-31 14:26:59 -0300

    Update 05-prototypes.js

--- curso-luizotavio-udemy/01-Fundamentos/04-objetos/05-prototypes.js
+++ curso-luizotavio-udemy/01-Fundamentos/04-objetos/05-prototypes.js
@@ -1,10 +1,14 @@
 // Construtora -> molde (classe)
 
+// prototype - tem em todas as funções construtoras, é ideal para armazenar métodos que serão utilizados varias vezes como o método de pegar o nome completo
+
 function Pessoa (nome,sobrenome) {
     this.nome = nome,
     this.sobrenome = sobrenome
-    this.nomeCompleto = this.nome + ' ' + this.sobrenome
+    Pessoa.prototype.nomeCompleto = () => this.nome + ' ' + this.sobrenome
 }
 
+
+
 const p1 = new Pessoa('Luan', 'da Silva ALbis')
-console.log(p1.nomeCompleto)
+console.log(p1.nomeCompleto())
