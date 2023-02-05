7a94244ee155e4ac2e62a4d3beab65f1c356c73c
LuanAlbis <luanalbis12@gmail.com>
2023-02-04 22:39:15 -0300

    Update 08-map.js

--- curso-luizotavio-udemy/01-Fundamentos/04-objetos/08-map.js
+++ curso-luizotavio-udemy/01-Fundamentos/04-objetos/08-map.js
@@ -1,2 +1,12 @@
 // map 
 
+const pessoas = [
+    {id: 3, nome:'Luan'},
+    {id: 2, nome:'Amanda'},
+    {id: 1, nome:'Paulo'}
+]
+
+const novasPessoas = {}
+
+for (const pessoa of pessoas)
+console.log(pessoa)
