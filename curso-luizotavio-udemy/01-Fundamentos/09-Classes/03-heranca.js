c12c490eb9ccfdd9797a5eb6a7398ba440facfc7
LuanAlbis <luanalbis12@gmail.com>
2023-02-06 19:21:44 -0300

    Update 03-heranca.js

--- curso-luizotavio-udemy/01-Fundamentos/09-Classes/03-heranca.js
+++ curso-luizotavio-udemy/01-Fundamentos/09-Classes/03-heranca.js
@@ -1,1 +1,32 @@
 //
+
+class DispositivoEletronico{
+    constructor(nome) {
+        this.nome = nome
+        this.ligado = false
+    }
+    ligar() {
+        if (this.ligado) return
+        this.ligado = true
+    }
+    desligar() {
+        if (!this.ligado) return
+        this.ligado = false
+    }
+}
+
+class Smartphone extends DispositivoEletronico{ // faz smartphone herdar tudo de outra class
+    constructor(nome,cor,modelo){
+        super(nome) // aqui estou dizzendo o parametros que minha class herdar
+        this.cor = cor
+        this.mmodelo = modelo
+    }
+}
+
+const sam1 = new Smartphone('Samsung' ,'Prata', 'SM8778000')
+sam1.ligado = true
+console.log(sam1)
+
+class Tablet extends DispositivoEletronico{
+    constructor(nome,mmmsmsmmsmmmmsmsmq)
+}
