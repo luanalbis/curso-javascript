7558ce0bee79c0584f3ec8f2a1353d62f7c6d07a
LuanAlbis <luanalbis12@gmail.com>
2023-02-06 21:36:53 -0300

    att

--- curso-luizotavio-udemy/01-Fundamentos/09-Classes/04-metodos-estaticos.js
+++ curso-luizotavio-udemy/01-Fundamentos/09-Classes/04-metodos-estaticos.js
@@ -1,7 +1,29 @@
 //
-class ConstroleRemoto {
+class ControleRemoto {
     constructor(nome){
         this.nome = nome
         this.volume = 0
     }
+
+    // METODO DE INSTANCIA // Metodos que serão utilizados por quem estanciou
+    aumentar () {
+        if (this.volume == 100) return
+        this.volume += 2
+    }
+
+    diminuir() {
+        if (this.volume == 0) return
+        this.volume -= 2
+    }
+
+    // METODO ESTÁTICO // Metodos utilizados pela classe, não terá acesso aos dados da instância
+    static trocaPilha (){
+        console.log('Precisa trocar a pilha')
+    }
 }
+
+let controle = new ControleRemoto('Controle Samsung')
+controle.aumentar()
+console.log(controle)
+
+ControleRemoto.trocaPilha() // método usado diretamente pela class
