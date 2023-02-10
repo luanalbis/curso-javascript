d3df112b0670b936c984187e55dbb41c277afcc6
LuanAlbis <luanalbis12@gmail.com>
2023-02-10 17:53:17 -0300

    att

--- curso-luizotavio-udemy/01-Fundamentos/10-promises/03-async-await.js
+++ curso-luizotavio-udemy/01-Fundamentos/10-promises/03-async-await.js
@@ -7,11 +7,34 @@ function espera(msg, tempo){ // parâmetros
         }
         setTimeout(() => { // aqui fazemos o set timeout que irá receber o resolve com o parametro
             resolve(msg)
-        }, tempo) // apos a função, coloque o tempo do timeout
+        },1000 ) // apos a função, coloque o tempo do timeout
     })
 }
 
-espera('Oi 1',1000)
+espera('Oi 1')
     .then(valor => {
-        console.log(valor)})
+        console.log(valor)
+        return espera('oi 2')})
+    .then(valor => {
+        console.log(valor)
+        return espera('oi 3')})
+
+    .then(valor => console.log(valor))
     .catch()
+
+async function executa() {
+    try {
+    console.log(await espera('oi 1'))
+    console.log(await espera('oi 2'))
+    console.log(await espera('oi 3'))
+
+    } catch (e) {console.log(e)}
+}
+
+executa()
+
+//pendind - pendente
+
+// fullfield - resolvida
+
+// rejected - rejeitada
