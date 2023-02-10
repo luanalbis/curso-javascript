6f1f18627d6f8bfeb0d2cc8ca0ca5fa1461c20f0
LuanAlbis <luanalbis12@gmail.com>
2023-02-10 18:24:31 -0300

    Update 04-ajax.js

--- curso-luizotavio-udemy/01-Fundamentos/10-promises/04-ajax.js
+++ curso-luizotavio-udemy/01-Fundamentos/10-promises/04-ajax.js
@@ -1,1 +1,12 @@
-//
+// AJAX
+
+const request = obj => {
+    const xhr = new XMLHttpRequest()
+    xhr.open(obj.method,obj.url, true)
+    xhr.send()
+
+    xhr.addEventListener ('load', () => {
+        if (xhr.status >= 200 && xhr.status < 300) {Object.success(xhr.responseText)}
+        else obj.error(xhr.statusText);
+    })
+}
